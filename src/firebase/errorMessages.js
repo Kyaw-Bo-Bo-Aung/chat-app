bc297e272429f8500c366c5cc7c0df0594b0d56f
Kyaw Bo Bo Aung <72304234+Kyaw-Bo-Bo-Aung@users.noreply.github.com>
2024-05-03 10:13:17 +0400

    Delete src/firebase directory

--- src/firebase/errorMessages.js
+++
@@ -1,5 +0,0 @@
-export const errorMessages = {
-  "auth/email-already-in-use": 'Email already exists.',
-  "auth/weak-password": 'Password must be at least 6 characters.',
-  "auth/invalid-credential": 'Invalid email or password',
-};
